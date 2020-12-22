dd3ab5bcf95418e99192c10c373d42c5de0854de
Zuzanna Usakiewicz <z.usakiewicz@gmail.com>
2020-12-22 11:35:37 +0100

    update theme colors

--- src/utils/theme.js
+++ src/utils/theme.js
@@ -1,7 +1,9 @@
 export const theme = {
   colors: {
-    main: "rgb(255,255,255)",
-    link: "#963b3b",
-    linkHover: "rgb(200, 200,200)",
+    main: "rgb(206, 206, 204)",
+    link: "rgb(29, 53, 87)",
+    linkHover: "rgb(29, 53, 87)",
+    fontColor: "rgb(116, 103, 96)",
+    navbarColor: "rgba(29, 53, 87, 0.9)",
   },
 }
