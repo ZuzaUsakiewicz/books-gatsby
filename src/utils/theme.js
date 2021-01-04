b3767cb4499c3c95f788e7bf37dc41ef6f7e013c
Zuzanna Usakiewicz <z.usakiewicz@gmail.com>
2021-01-04 21:52:48 +0100

    add new theme colors

--- src/utils/theme.js
+++ src/utils/theme.js
@@ -1,9 +1,14 @@
 export const theme = {
   colors: {
+    backgroundColor: "rgb(37, 37, 37)",
     main: "rgb(206, 206, 204)",
-    link: "rgb(29, 53, 87)",
-    linkHover: "rgb(29, 53, 87)",
+    link: "rgb(222, 228, 238)",
+    linkHover: "rgb(17, 32, 53)",
     fontColor: "rgb(116, 103, 96)",
-    navbarColor: "rgba(29, 53, 87, 0.9)",
+    navbarColor: "rgba(0, 0, 0, 0.9)",
+    black: "rgb(4, 2, 14)",
+  },
+  boxShadow: {
+    main: "2px 2px 2px #000",
   },
 }
