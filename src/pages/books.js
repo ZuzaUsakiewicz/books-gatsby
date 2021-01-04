826c9c976ee4d8e0af26efb12875a6b672d02152
Zuzanna Usakiewicz <z.usakiewicz@gmail.com>
2021-01-04 21:50:11 +0100

    add new page

---
+++ src/pages/books.js
@@ -0,0 +1,14 @@
+import React from "react"
+import bookphoto from "../images/bookbg.jpg"
+import {
+  BookPhoto,
+  BookContainer,
+} from "../components/styles/HeroSection.styled"
+
+export default function Books() {
+  return (
+    <BookContainer>
+      <h1>Books opinion</h1>
+    </BookContainer>
+  )
+}
