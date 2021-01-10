19c4acac0dff0e7840446f896b69116e39b8cfaa
Zuzanna Usakiewicz <z.usakiewicz@gmail.com>
2021-01-10 12:14:26 +0100

    create template for books page

--- src/pages/books.js
+++ src/pages/books.js
@@ -1,14 +1,42 @@
 import React from "react"
-import bookphoto from "../images/bookbg.jpg"
-import {
-  BookPhoto,
-  BookContainer,
-} from "../components/styles/HeroSection.styled"
+import { BookContainer } from "../components/styles/HeroSection.styled"
+import AniLink from "gatsby-plugin-transition-link/AniLink"
+import { graphql } from "gatsby"
 
-export default function Books() {
+export default function Books({ data }) {
   return (
     <BookContainer>
       <h1>Books opinion</h1>
+      {data.allMarkdownRemark.edges.map(({ node }) => (
+        <div key={node.id}>
+          <AniLink
+            paintDrip
+            to="/"
+            hex="#000"
+            duration={1}
+            to={node.frontmatter.slug}
+          >
+            Go to my {node.frontmatter.title} blog post
+          </AniLink>
+        </div>
+      ))}
     </BookContainer>
   )
 }
+export const query = graphql`
+  query MyQuery {
+    allMarkdownRemark {
+      totalCount
+      edges {
+        node {
+          id
+          frontmatter {
+            date
+            slug
+            title
+          }
+        }
+      }
+    }
+  }
+`
