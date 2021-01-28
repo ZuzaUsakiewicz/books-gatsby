89a43dd2b59c23674098203caedab662fa45976d
Zuzanna Usakiewicz <z.usakiewicz@gmail.com>
2021-01-28 16:51:06 +0100

    change hero section styles

--- src/components/styles/HeroSection.styled.js
+++ src/components/styles/HeroSection.styled.js
@@ -8,32 +8,62 @@ export const HeroContainer = styled.section`
   flex-direction: column;
   align-items: center;
   padding: 5rem 1rem 0rem 1rem;
+  @media screen and (min-width: 768px) {
+    flex-direction: row;
+  }
 `
 export const Title = styled.figure`
-  padding: 0.5rem;
+  padding: 1rem 1rem 0.3rem 1rem;
   display: flex;
   flex-direction: column;
   justify-content: center;
-  text-align: justify;
   align-items: center;
   h1 {
-    font-size: 1.5rem;
+    font-size: 1.6rem;
+
+    text-transform: uppercase;
+    font-style: italic;
   }
   p {
     font-size: 1rem;
+    text-align: justify;
+  }
+  @media screen and (min-width: 768px) {
+    h1,
+    p {
+      color: ${({ theme }) => theme.colors.main};
+    }
   }
 `
 export const ReadingImg = styled.img`
   width: 100%;
+  @media screen and (min-width: 768px) {
+    width: 50%;
+    padding: 0 0.5rem;
+  }
 `
+export const ButtonsContainer = styled.div`
+  display: flex;
+  width: 100%;
+  justify-content: space-evenly;
+  @media screen and (min-width: 768px) {
+    width: 50%;
+  }
+`
+
 export const BackgroundDiv = styled.div`
   position: absolute;
   top: 0;
   left: 0;
   right: 0;
-  background-color: #0093e9;
-  background-image: linear-gradient(160deg, #0093e9 0%, #80d0c7 100%);
+  background-color: ${({ theme }) => theme.colors.navbarColor};
+  background-image: ${({ theme }) =>
+    `linear-gradient(160deg, ${theme.colors.navbarColor} 0%, ${theme.colors.gradientSecond} 100%)`};
   z-index: -1;
-  clip-path: ellipse(70% 20% at 35% 17%);
-  height: 70%;
+  height: 70vh;
+  border-bottom: 2px solid #991383;
+  @media screen and (min-width: 768px) {
+    height: 100vh;
+    border: none;
+  }
 `
