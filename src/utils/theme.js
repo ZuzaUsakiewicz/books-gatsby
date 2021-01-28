e1241d85d1a31064c61152fa013336240dc6eaa1
Zuzanna Usakiewicz <z.usakiewicz@gmail.com>
2021-01-28 16:51:54 +0100

    change colors

--- src/utils/theme.js
+++ src/utils/theme.js
@@ -1,14 +1,13 @@
 export const theme = {
   colors: {
-    backgroundColor: "rgb(37, 37, 37)",
-    main: "rgb(206, 206, 204)",
-    link: "rgb(222, 228, 238)",
-    linkHover: "rgb(17, 32, 53)",
-    fontColor: "rgb(116, 103, 96)",
-    navbarColor: "rgba(0, 0, 0, 0.9)",
-    black: "rgb(4, 2, 14)",
+    backgroundColor: "rgb(247, 217, 233)",
+    main: "rgb(243, 243, 240)",
+    link: "rgb(227, 234, 248)",
+    navbarColor: "rgba(6, 129, 201, 0.822)",
+    navbarSlider: "rgb(6, 129,201)",
+    gradientSecond: "rgb(199, 16, 144)",
   },
   boxShadow: {
-    main: "2px 2px 2px #000",
+    main: "2px 2px 4px rgba(0,0,0,0.4)",
   },
 }
